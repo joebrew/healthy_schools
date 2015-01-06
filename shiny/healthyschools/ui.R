fe79830ef2ef80c3f87eab56db4efb69edbcd9b9
Joe Brew <joebrew@gmail.com>
2015-01-05 19:17:46 -0500

    app for meeting with surgeon general

--- shiny/healthyschools/ui.R
+++ shiny/healthyschools/ui.R
@@ -48,14 +48,14 @@ shinyUI(fluidPage(
       tabsetPanel(id="tabs",
                   tabPanel("Presentation",
                            tags$div(
-                             HTML('<iframe src="https://docs.google.com/presentation/d/1JC_B_RFjfPZ1DoloUpvTdZFbmysbQ-iR_eNVDAzbmk4/embed?start=false&loop=false&delayms=60000" frameborder="0" width="480" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>')
+                             HTML('<iframe src="https://docs.google.com/presentation/d/1JC_B_RFjfPZ1DoloUpvTdZFbmysbQ-iR_eNVDAzbmk4/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>')
                            )
                           # htmlOutput("motionchart1") 
                           ),
                   
 
                   tabPanel("Explore the data",
-                           p("These data come from the Human Mortality Database, but the visualizations are original."),
+                           p("Projections from Weycker D, Edelsberg J, Halloran ME, et al. Population-wide benefits of routine vaccination of children against influenza. Vaccine 2005; 23(10): 1284-93."),
                            plotOutput("plot1"),
                            plotOutput("plot2"),
                            plotOutput("plot3")
